d2255f9c6d40820d6690d3edd1aa8494435f7b3e
kikiriko200 <fujitakakikiriko200@yahoo.co.jp>
2020-01-05 16:40:51 +0900

    add files

---
+++ temp/head.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta name="author" content="dafu">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta name="theme-color" content="#aaa">
+    <link rel="canonical" href="https://cdn.dafu.cf/?f=riot">
+    <link rel="manifest" href="https://cdn.dafu.cf/assets/js/manifest.json">
+    <link rel="preload" as="style" href="https://cdn.dafu.cf/assets/css/style.min.css">
+    <noscript>
+      <link rel="stylesheet" href="https://cdn.dafu.cf/assets/css/style.min.css">
+    </noscript>
+    <script src="https://cdn.dafu.cf/assets/js/serviceworker.js"></script>
+  </head>
